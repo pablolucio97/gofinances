8525b368106ac998de6339cbd7cf6b9c9bf975fc
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-01-14 18:54:40 -0300

    Feature: Added loading indicator to fetch data from async storage

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -3,8 +3,10 @@ import { useFocusEffect } from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import 'intl'
 import 'intl/locale-data/jsonp/pt-BR'
+import { ActivityIndicator } from 'react-native'
 
 import { HihghlightCard } from '../../components/HighlightCard'
+import { useTheme } from 'styled-components'
 import { TransactionCard, TransactionProps } from '../../components/TransactionCard'
 import {
     Container,
@@ -21,6 +23,7 @@ import {
     Title,
     TransactionsList,
     LogoutButton,
+    LoadingContainer
 } from './styles'
 
 import { ASYNC_STORAGE_TRANSACTIONS_KEY, GITHUB_PROFILE_URL } from '../../utils/constants'
@@ -29,23 +32,45 @@ export interface DataListProps extends TransactionProps {
     id: string;
 }
 
+interface HighlightCardProps {
+    amount: string;
+}
+
+interface HilighDataCardProp {
+    entries: HighlightCardProps;
+    expansives: HighlightCardProps;
+    total: HighlightCardProps
+}
+
 export function Dashboard() {
 
+    const theme = useTheme()
 
+    const [trasactions, setTransactions] = useState<DataListProps[]>([])
+    const [highlighData, setHighlighData] = useState<HilighDataCardProp>({} as HilighDataCardProp)
+    const [isLoading, setIsLoading] = useState(true)
 
-    const [data, setData] = useState<DataListProps[]>([])
-
+    let entriesTotal = 0;
+    let expansivesTotal = 0;
 
 
     async function loadTransactions() {
         const response = await AsyncStorage.getItem(ASYNC_STORAGE_TRANSACTIONS_KEY)
         const transactions = response ? JSON.parse(response) : []
 
+
         const formatedTransactions: DataListProps[] = transactions.map((transaction: DataListProps) => {
             const amount = Number(transaction.amount).toLocaleString('ptBR', {
                 style: 'currency',
                 currency: 'BRL'
             })
+
+
+            if (transaction.type === 'positive') {
+                entriesTotal += Number(transaction.amount)
+            } else {
+                expansivesTotal += Number(transaction.amount)
+            }
 
             const date = Intl.DateTimeFormat('pt-BR', {
                 day: '2-digit',
@@ -64,8 +89,32 @@ export function Dashboard() {
 
         })
 
+        const total = Number(entriesTotal - expansivesTotal)
 
-        setData(formatedTransactions)
+        setTransactions(formatedTransactions)
+        setHighlighData({
+            entries: {
+                amount: entriesTotal.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+            expansives: {
+                amount: expansivesTotal.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+            total: {
+                amount: total.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+        })
+
+        setIsLoading(false)
+
     }
 
     useEffect(() => {
@@ -79,61 +128,72 @@ export function Dashboard() {
 
     return (
         <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo
-                            source={{ uri: GITHUB_PROFILE_URL }}
+            {isLoading ?
+                <LoadingContainer>
+                    <ActivityIndicator
+                        color={theme.colors.secondary}
+                        size='large'
+                    />
+                </LoadingContainer>
+                :
+                <>
+                    <Header>
+                        <UserWrapper>
+                            <UserInfo>
+                                <Photo
+                                    source={{ uri: GITHUB_PROFILE_URL }}
+                                />
+                                <User>
+                                    <UserGreetings>Olá,</UserGreetings>
+                                    <UserName>Pablo Silva</UserName>
+                                </User>
+                            </UserInfo>
+                            <LogoutButton onPress={() => { }}>
+                                <PowerIcon name='power' />
+                            </LogoutButton>
+                        </UserWrapper>
+                    </Header>
+                    <HighlightCards
+
+                    >
+                        <HihghlightCard
+                            title='Entradas'
+                            amount={highlighData?.entries?.amount}
+                            lastTransaction='8 de janeiro de 2022'
+                            type='up'
                         />
-                        <User>
-                            <UserGreetings>Olá,</UserGreetings>
-                            <UserName>Pablo Silva</UserName>
-                        </User>
-                    </UserInfo>
-                    <LogoutButton onPress={() => { }}>
-                        <PowerIcon name='power' />
-                    </LogoutButton>
-                </UserWrapper>
-            </Header>
-            <HighlightCards
-
-            >
-                <HihghlightCard
-                    title='Entrada'
-                    amount='R$17.400,00'
-                    lastTransaction='8 de janeiro de 2022'
-                    type='up'
-                />
-                <HihghlightCard
-                    title='Saída'
-                    amount='R$300,00'
-                    lastTransaction='28 de fervereiro de 2021'
-                    type='down'
-                />
-                <HihghlightCard
-                    title='Total'
-                    amount='R$17.100,00'
-                    lastTransaction='8 de janeiro de 2021'
-                    type='total'
-                />
-            </HighlightCards>
-            <Transactions>
-                <Title>Listagem</Title>
-                <TransactionsList
-                    //@ts-ignore
-                    data={data}
-                    //@ts-ignore
-                    keyExtractor={item => item.id}
-                    //@ts-ignore
-                    renderItem={({ item }) => (
-                        <TransactionCard
-                            data={item}
+                        <HihghlightCard
+                            title='Saídas'
+                            amount={highlighData?.expansives?.amount}
+                            lastTransaction='28 de fervereiro de 2021'
+                            type='down'
                         />
-                    )}
-                />
+                        <HihghlightCard
+                            title='Total'
+                            amount={highlighData?.total?.amount}
+                            lastTransaction='8 de janeiro de 2021'
+                            type='total'
+                        />
+                    </HighlightCards>
+                    <Transactions>
+                        <Title>Listagem</Title>
+                        <TransactionsList
+                            //@ts-ignore
+                            data={trasactions}
+                            //@ts-ignore
+                            keyExtractor={item => item.id}
+                            //@ts-ignore
+                            renderItem={({ item }) => (
+                                <TransactionCard
+                                    data={item}
+                                />
+                            )}
+                        />
 
 
-            </Transactions>
+                    </Transactions>
+                </>
+            }
         </Container>
     )
 }
