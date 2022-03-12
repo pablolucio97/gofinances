938bcf87ea241940f3985519a94197bd0cf8abee
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-03-12 17:48:47 -0300

    🛠 Fixes: Fixed constants user messages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 export const GITHUB_PROFILE_URL =
   "https://avatars.githubusercontent.com/u/54117323?s=400&u=fc2e6db866d186d0e26aba123cd1d9f5b370ca39&v=4";
-export const TITLE_OBLIGATORY_FILED = "Campo obrigtório";
+export const TITLE_OBLIGATORY_FILED = "Campo obrigtório.";
 export const TRANSACTION_TYPE_INPUT_MISSING = "Selecione o tipo da transação.";
 export const CATEGORY_INPUT_MISSING = "Selecione a categoria.";
-export const ITEM_INPUT_MISSING = "Item obrigatório";
-export const PRICE_INPUT_MISSING = "Preço obrigatório";
-export const PRICE_SHOULD_BE_POSITIVE = "Somente valores positivos são aceitos";
+export const ITEM_INPUT_MISSING = "Item obrigatório.";
+export const PRICE_INPUT_MISSING = "Preço obrigatório.";
+export const PRICE_SHOULD_BE_POSITIVE = "Somente valores positivos são aceitos.";
 export const ASYNC_STORAGE_ERROR_TO_SAVE = 'Não foi possível salvar os dados no dispositivo.'
 export const ASYNC_STORAGE_TRANSACTIONS_KEY = '@gofinances:transactions:user:'
 export const ASYNC_STORAGE_USER_AUTH_KEY = '@gofinances:user'
-export const GOOGLE_AUTH_ERROR = 'Não foi possível acessar a conta Google'
-export const APPLE_AUTH_ERROR = 'Não foi possível acessar a conta Apple'
+export const GOOGLE_AUTH_ERROR = 'Não foi possível acessar a conta Google.'
+export const APPLE_AUTH_ERROR = 'Não foi possível acessar a conta Apple.'
